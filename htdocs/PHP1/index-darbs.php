59bd4a8f234bfbc3f7819e858d3ad13cd66c4f0e
VitalijsMuzalovs <102912469+VitalijsMuzalovs@users.noreply.github.com>
2022-11-29 17:53:10 +0200

    a

---
+++ htdocs/PHP1/index-darbs.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="lv">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Muzalovs</title>
+</head>
+<body>
+
+<?php
+echo "<h1>1.uzd</h1>";
+$a = array(7,8,9,10,11,12);
+echo $a[4];
+echo "<hr>";
+
+echo "<h1>2.uzd</h1>";
+$b = array("viens" => 1, "divi" => 2, "trīs" => 3, "četri" => 4);
+echo $b['trīs'];
+echo "<hr>";
+
+echo "<h1>3.uzd</h1>";
+$skaitli = [2,3,4];
+array_push($skaitli,5);
+echo "Skaitļa 5 indekss:".array_search(5,$skaitli);
+echo "<hr>";
+
+echo "<h1>4.uzd</h1>";
+$divdimensiju[1][] = "viens";
+$divdimensiju[1][] = "divi";
+$divdimensiju[1][] = "trīs";
+$divdimensiju[2][] = "četri";
+$divdimensiju[2][] = "pieci";
+$divdimensiju[2][] = "seši";
+
+echo "3: ".$divdimensiju[1][2];
+echo "<br>";
+echo "5: ".$divdimensiju[2][1];
+echo "<hr>";
+
+echo "<h1>5.uzd</h1>";
+$augusts =[22.5,23.6,27.1,27.3,27.5,27.9,28.0,28.6,28.8];
+rsort($augusts);
+foreach($augusts as $num){
+    echo $num."<br>";
+}
+echo "<hr>";
+
+echo "<h1>6.uzd</h1>";
+$draugi = array('Viktors'=>17,'Aldis'=>19,'Ansis'=>26,'Miks'=>23,);
+arsort($draugi);
+
+foreach ($draugi as $key => $value){
+    echo $key." ir ".$value." gadus vecs. <br>";
+}
+echo "<hr>";
+
+echo "<h1>7.uzd</h1>";
+
+$profesijas = array(
+array('profesija'=>'Programmēšanas tehniķis','text'=>'Programmēšanas tehniķis piedalās programmatūras projektēšanā, ieviešanā un uzturēšanā, kā arī jau esošas programmatūras pielagošanā konkrētā pasūtītāja vajadzībām. Programmēšanas tehniķis apkalpo datorsistēmas un programmatūru, instalē un konfigurē esošo un vēlamo programmatūru.','pamatalga'=> '1000 EUR','vakances'=> 3, 'slodze'=> 'Pilna (8h)'),
+
+array('profesija'=>'Datorsistēmu tehniķis','text'=>'Brīvi pārvalda biroja lietojumprogrammas dokumentu, aprēķinu tabulu, prezentāciju sagatavošanai un datorlietotāja darba procesu automatizēšanaim sagatavo un uztur vienkāršas datubāzes un interneta mājas lapas.','pamatalga'=> '850 EUR','vakances'=> 3, 'slodze'=> 'Pilna (8h)'),
+
+array('profesija'=>'Elektrotehniķis','text'=>'Veic ārējo un iekšējo elektrisko tīklu izbūvi un elektroietaišu ierīkošanas, ekspluatācijas un remonta darbus - labo dažādas elektrīskās iekārtas - sadzīvs tehniku, apgaismes objektus, elektriskos sadalītājus, elektromotrus, ģeneratorus, slodzes un jaudas slēdžus u.c. iekārtas.','pamatalga'=> '800 EUR','vakances'=> 1, 'slodze'=> 'Pilna (8h)'),
+
+array('profesija'=>'Mehatronisku sistēmu tehniķis','text'=>'Mehatronisku sistēmu tehniķis veic elektropiedziņas un iekārtu vadības elektrisko shēmu montāžu un regulēšanu.Mehatronisku sistēmu tehniķis veic individuālu darbu saistībā ar automatizētu un datorizētu vadāmu tehnoloģisku procesu tehnisko apkalpošanu, ierīču montāžu un regulēšan, veic defektu atklāšanu un novertēšanu.','pamatalga'=> '500 EUR','vakances'=> 2, 'slodze'=> 'Nepilna (4h)'));
+?>
+
+<style>
+.grid-item{
+    display:grid;
+    grid-template-columns: 1fr 1fr;
+    grid-auto-rows: 1fr;
+    width: 80%;
+    background-color: #eee;
+    font-family: Calibri;
+    font-size: 18px;
+    margin: 0 auto;
+}
+
+.article{
+    display: grid;
+    grid-auto-flow: row;
+    grid-template: 20% 1fr auto auto / 1fr;
+    align-content: flex-start;
+    margin: 10px;
+    background-color: #ddd;
+    font-size: 18px;
+}
+
+h2{
+    font-size: 21px;
+    text-align: center;
+}
+
+.description{
+    margin: 10px;
+    text-align:justify;
+}
+
+.details{
+    text-align: center;
+    font-size: 16px;
+}
+
+.btn{
+    justify-self:stretch; 
+    cursor: pointer;
+    margin:10px;
+    padding: 1% 0;
+    background: #333;
+    text-align: center;
+    border-radius: 5px;
+    color:#fff;
+}
+
+
+</style>
+
+<div class="container">
+    <div class="grid-item">
+        <?php
+        foreach($profesijas as $item){
+            echo    
+            "<div class='article'>
+                <div><h2>".$item['profesija']."</h2></div>
+
+                <div class='description'>".$item['text']."</div>
+
+                <div class='details'>
+                    Pamatalga: ".$item['pamatalga']." | 
+                    Vakances: ".$item['vakances']." | 
+                    Slodze: ".$item['slodze']." 
+                </div>
+
+                <div class='btn'>PIETEIKTIES</div>
+            </div>";
+        }
+        ?>
+    </div>
+</div>
+
+</body>
+</html>
--- htdocs/PHP1/index.php
+++ htdocs/PHP1/index.php
@@ -4,142 +4,262 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Muzalovs</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="script.js"></script>
+    <title>Ievāds PHP</title>
+    <link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
 </head>
 <body>
+    <div class="container">
+        <div class="flex-item">
+            <ul>
+                <a onclick="showArticle('article1')"><img id="uzdevums1" src="images/one.png"></a>
+                <a onclick="showArticle('article2')"><img id="uzdevums2" src="images/two.png"></a>
+                <a onclick="showArticle('article3')"><img id="uzdevums3" src="images/three.png"></a>
+                <a onclick="showArticle('article4')"><img id="uzdevums4" src="images/four.png"></a>
+                <a onclick="showArticle('article5')"><img id="uzdevums5" src="images/five.png"></a>
+            </ul>
 
-<?php
-echo "<h1>1.uzd</h1>";
-$a = array(7,8,9,10,11,12);
-echo $a[4];
-echo "<hr>";
+            <article id="article1">
+                <?php
+                    echo "<h1>Title 1 PHP</h1><hr>";
+                    echo "Teksta izvade ar ECHO.<br>";
+                    print "Teksta izvade ar PRINT.<br>";
+                    $zinojums = "Teksts, kurš ievietots mainīgajā ZIŅOJUMS";
+                ?>
 
-echo "<h1>2.uzd</h1>";
-$b = array("viens" => 1, "divi" => 2, "trīs" => 3, "četri" => 4);
-echo $b['trīs'];
-echo "<hr>";
+                <p><?php echo $zinojums; ?></p>
+                
+                <?php 
+                    $userName = "Vital";
+                    $birthYear = 1984;
+                    $thisYear = 2022;
+                    $age = $thisYear - $birthYear;
 
-echo "<h1>3.uzd</h1>";
-$skaitli = [2,3,4];
-array_push($skaitli,5);
-echo "Skaitļa 5 indekss:".array_search(5,$skaitli);
-echo "<hr>";
+                    echo "$userName, you are $age years old! <br>";
+                    echo 'User name: $userName <br>';
+                    echo "User name: $userName <br>";
 
-echo "<h1>4.uzd</h1>";
-$divdimensiju[1][] = "viens";
-$divdimensiju[1][] = "divi";
-$divdimensiju[1][] = "trīs";
-$divdimensiju[2][] = "četri";
-$divdimensiju[2][] = "pieci";
-$divdimensiju[2][] = "seši";
+                    echo "Lietotājvārds: $userName (dzimšanas gads: $birthYear)<br>";
+                    echo 'Lietotājvārds:'.$userName.'(dzimšanas gads: '.$birthYear.')<br>';
 
-echo "3: ".$divdimensiju[1][2];
-echo "<br>";
-echo "5: ".$divdimensiju[2][1];
-echo "<hr>";
+                    define("Pi",3.14159);
+                    echo 'Defined constant: '.Pi.'<br>';
+                    echo 'Rounded Pi: '.round(Pi,2).'<br>';
 
-echo "<h1>5.uzd</h1>";
-$augusts =[22.5,23.6,27.1,27.3,27.5,27.9,28.0,28.6,28.8];
-rsort($augusts);
-foreach($augusts as $num){
-    echo $num."<br>";
-}
-echo "<hr>";
+                    $uzraksts = "Es tagad mācos PHP";
+                    echo var_dump($uzraksts);
 
-echo "<h1>6.uzd</h1>";
-$draugi = array('Viktors'=>17,'Aldis'=>19,'Ansis'=>26,'Miks'=>23,);
-arsort($draugi);
+                    $skaitlis = 68.456;
+                    echo var_dump($skaitlis);
 
-foreach ($draugi as $key => $value){
-    echo $key." ir ".$value." gadus vecs. <br>";
-}
-echo "<hr>";
+                    echo '<br> Random skaitlis: '.rand(10,100).'<br>';
 
-echo "<h1>7.uzd</h1>";
+                    echo 'Simbolu skaits:'.mb_strlen($uzraksts).'<br>';
+                    echo 'Vārdu skaits: '.str_word_count($uzraksts).'<br>';
+                ?>
 
-$profesijas = array(
-array('profesija'=>'Programmēšanas tehniķis','text'=>'Programmēšanas tehniķis piedalās programmatūras projektēšanā, ieviešanā un uzturēšanā, kā arī jau esošas programmatūras pielagošanā konkrētā pasūtītāja vajadzībām. Programmēšanas tehniķis apkalpo datorsistēmas un programmatūru, instalē un konfigurē esošo un vēlamo programmatūru.','pamatalga'=> '1000 EUR','vakances'=> 3, 'slodze'=> 'Pilna (8h)'),
+                <hr>
+                <form action="" method="post">
+                    <input class="ievade" type="text" name="Ievade">
+                    <input class="btn" type="submit" name="Iesniegt" value="Send">
+                </form>
 
-array('profesija'=>'Datorsistēmu tehniķis','text'=>'Brīvi pārvalda biroja lietojumprogrammas dokumentu, aprēķinu tabulu, prezentāciju sagatavošanai un datorlietotāja darba procesu automatizēšanaim sagatavo un uztur vienkāršas datubāzes un interneta mājas lapas.','pamatalga'=> '850 EUR','vakances'=> 3, 'slodze'=> 'Pilna (8h)'),
+                <?php 
+                    $teksts = '';
+                    if(isset($_POST['Iesniegt'])){
+                        $teksts = $_POST['Ievade'];
+                        //echo "<script>showArticle('article1')</script>";
+                        echo "<script>document.getElementById('article1').style.display ='block'</script>";
 
-array('profesija'=>'Elektrotehniķis','text'=>'Veic ārējo un iekšējo elektrisko tīklu izbūvi un elektroietaišu ierīkošanas, ekspluatācijas un remonta darbus - labo dažādas elektrīskās iekārtas - sadzīvs tehniku, apgaismes objektus, elektriskos sadalītājus, elektromotrus, ģeneratorus, slodzes un jaudas slēdžus u.c. iekārtas.','pamatalga'=> '800 EUR','vakances'=> 1, 'slodze'=> 'Pilna (8h)'),
+                        if(is_numeric($teksts)){
+                            echo $teksts.' ir skaitlis<br>';
+                        }else if($teksts == ''){
+                            echo 'Ievades lauks ir tukšs!<br>';
+                        }else{
+                            echo "'".$teksts."' ir teksts!<br>";
+                        }
+                    }
+                ?>
 
-array('profesija'=>'Mehatronisku sistēmu tehniķis','text'=>'Mehatronisku sistēmu tehniķis veic elektropiedziņas un iekārtu vadības elektrisko shēmu montāžu un regulēšanu.Mehatronisku sistēmu tehniķis veic individuālu darbu saistībā ar automatizētu un datorizētu vadāmu tehnoloģisku procesu tehnisko apkalpošanu, ierīču montāžu un regulēšan, veic defektu atklāšanu un novertēšanu.','pamatalga'=> '500 EUR','vakances'=> 2, 'slodze'=> 'Nepilna (4h)'));
-?>
+                <hr>
 
-<style>
-.grid-item{
-    display:grid;
-    grid-template-columns: 1fr 1fr;
-    grid-auto-rows: 1fr;
-    width: 80%;
-    background-color: #eee;
-    font-family: Calibri;
-    font-size: 18px;
-    margin: 0 auto;
-}
+                <form action="" method="post">
+                    <input class="ievade" type="number" name="laikaIevade">
+                    <input class="btn" type="submit" name="laikaIesniegsana" value="Convert">
+                </form>
 
-.article{
-    display: grid;
-    grid-auto-flow: row;
-    grid-template: 20% 1fr auto auto / 1fr;
-    align-content: flex-start;
-    margin: 10px;
-    background-color: #ddd;
-    font-size: 18px;
-}
+                
+                    
+                <?php
+                $sekundes = 0;
+                $minutes = 0;
+                $stundas = 0;
 
-h2{
-    font-size: 21px;
-    text-align: center;
-}
+                $teksts = '';
+                if(isset($_POST['laikaIesniegsana'])){
+                    $sekundes = $_POST['laikaIevade'];
+                    //echo "<script>showArticle('article1')</script>";
+                    echo "<script>document.getElementById('article1').style.display ='block'</script>";
+                    if(is_numeric($sekundes)){
+                        $stundas =  $sekundes /3600;
+                        $minutes = (int) ($sekundes /60) %60;
+                        $sekundes = $sekundes %60;
+                    }
+                    echo round($stundas).'h:'.round($minutes).'m:'.round($sekundes).'s';
+                }
+    
+                ?>
 
-.description{
-    margin: 10px;
-    text-align:justify;
-}
+            </article>
+            
+            <article id="article2">
+                <h1>PHP masīvi</h1>
+                <?php
+                    $komponents = array("processors","mātes plate","videokarte","RAM","barošanas bloks");
+                    echo "<b>Datora sastāvdaļas</b> ir: $komponents[0],$komponents[1],$komponents[2],$komponents[4] u.c. <br>";
+                    
+                    print_r ($komponents);
 
-.details{
-    text-align: center;
-    font-size: 16px;
-}
+                    echo "<br><br>";
 
-.btn{
-    justify-self:stretch; 
-    cursor: pointer;
-    margin:10px;
-    padding: 1% 0;
-    background: #333;
-    text-align: center;
-    border-radius: 5px;
-    color:#fff;
-}
+                    $koki = array(
+                        array("Priede","skujkoks"),
+                        array("Ozols","lapu koks"),
+                        array("Egle","skujkoks"),
+                    );
+                    
+                    //var_dump($koki);
+
+                    echo "* ".$koki[0][0]." ir koks, kas skaitās ".$koki[0][1]."<br>";
+                    echo "* ".$koki[1][0]." ir koks, kas skaitās ".$koki[1][1]."<br>";
+                    echo "* ".$koki[2][0]." ir koks, kas skaitās ".$koki[2][1]."<br><hr>";
+
+                    foreach($koki as $koks){
+                        echo "* ".$koks[0]." ir koks, kas skaitās ".$koks[1]."<br>";
+                    }
+
+                    echo "<hr>";
+
+                    $audzekni = array(
+                        array('vards'=>'Jānis','uzvards'=>'Bērziņš','telefons'=>28888888),
+                        array('vards'=>'Juris','uzvards'=>'Strautmanis','telefons'=>27777777),
+                        array('vards'=>'Anna','uzvards'=>'Panna','telefons'=>26666666),
+                        array('vards'=>'Zane','uzvards'=>'Bērziņa','telefons'=>25555555),
+                    );
+
+                    echo "
+                    <table>
+                        <tr>
+                            <th>Vārds</th>
+                            <th>Uzvārds</th>
+                            <th>Tālrunis</th>
+                        </tr>
+                        ";
+                        foreach($audzekni as $audzeknis){
+                            echo "
+                            <tr>
+                                <td>".$audzeknis['vards']."</td>
+                                <td>".$audzeknis['uzvards']."</td>
+                                <td>".$audzeknis['telefons']."</td>
+                            </tr>
+                        ";
+                        }
+                    echo "</table>";
+
+                ?>
+            </article>
+            
+            <article id="article3">
+                <h1>PHP kontroles struktūras</h1>
+                <hr>
+                <p>Norādi gadalaiku:</p>
+                <form action="" method="post">
+                    <input class="ievade" type="text" name="gadalaikaIevade">
+                    <input class="btn" type="submit" name="gadalaikaIesniegsana" value="Asociēti!">
+                </form>
+                
+                <?php
+                    $gadalaiks = '';
+                    if(isset($_POST['gadalaikaIesniegsana'])){
+                        echo "<script>showArticle('article3')</script>";
+                        $gadalaiks = $_POST['gadalaikaIevade'];
+                    }
+
+                    if(!empty($gadalaiks)){
+                        switch(mb_strtolower($gadalaiks)){
+                            case 'ziema':
+                                echo 'Ziema asociējas ar sniegu un ledu.';
+                                break;
+                            case 'pavasaris':
+                                echo 'Pavasaris asociējas ar pumpuriem un lapu plaukšanu.';
+                                break;
+                            case 'vasara':
+                                echo 'Vasara asociējas ar sauli un karstumu.';
+                                break;
+                            case 'rudens':
+                                echo 'Rudens asociējas ar dzeltenajām lapām un dubļiem.';
+                            default:
+                                echo 'Diemžēl neatpazinu gadalaiku! :(';
+                                break;
+                        }
+                    }
+                    echo "<hr>";
+                    echo "<p>Šahu laukuma izveide:</p>";
+                    echo "<table style='width:400px; height:350px'>";
+                    for($rinda=1; $rinda<=8;$rinda++){
+                        echo "<tr>";
+                            for($kolonna=1;$kolonna<=8;$kolonna++){
+                                $kopa = $rinda + $kolonna;
+                                if($kopa%2==0){
+                                    echo "<td style='background: #fff;'></td>";
+                                }else{
+                                    echo "<td style='background: #000;'></td>";
+                                }
+                            }
+                        echo "</tr>";
+                    }
+                    echo "</table>";
+
+                    echo "<hr>";
+                    echo "<p>100:</p>";
+                    echo "<table style='width:600px; height:600px'>";
+                    for($rinda=1; $rinda<=10;$rinda++){
+                        echo "<tr>";
+                            for($kolonna=1;$kolonna<=10;$kolonna++){
+                                $kopa = $rinda * $kolonna;
+                                echo "<td style='border: 1px solid #555; color:#000;'>". $kopa ."</td>";
+                            }
+                        echo "</tr>";
+                    }
+                    echo "</table>";
+
+                    echo "<hr>";
+                    echo "<h1>PHP funkcijas </h1><hr>";
+                    
+                    function zinojums(){
+                        echo "Mana pirmā PHP funkcija";
+                    }
+
+                    zinojums();
+                    echo "<br><p>Funkcija ar argumentiem jeb parametriem:</p>";
+                    
+                    function audzekni($vards,$uzvards,$kurss){
+                        echo "Audzēknis <b>$vards $uzvards</b> mācās kursā: $kurss <br>";
+                    }
+
+                    audzekni("Liene","Ozola","Pavārs");
+                    audzekni("Jānis","Bērziņš","Konditors");
+                    audzekni("Andris","Lapa","Programetājs");
+                ?>
+            </article>
 
 
-</style>
-
-<div class="container">
-    <div class="grid-item">
-        <?php
-        foreach($profesijas as $item){
-            echo    
-            "<div class='article'>
-                <div><h2>".$item['profesija']."</h2></div>
-
-                <div class='description'>".$item['text']."</div>
-
-                <div class='details'>
-                    Pamatalga: ".$item['pamatalga']." | 
-                    Vakances: ".$item['vakances']." | 
-                    Slodze: ".$item['slodze']." 
-                </div>
-
-                <div class='btn'>PIETEIKTIES</div>
-            </div>";
-        }
-        ?>
+            <article id="article4"><h1>Title 4</h1></article>
+            <article id="article5"><h1>Title 5</h1></article>
+        </div>
     </div>
-</div>
 
+    <footer>Izstrādāja Vitālijs Muzaļovs &copy; 2022</footer>
 </body>
 </html>
