b4d5893514122fbf72234759b6423511c44ac655
VitalijsMuzalovs <102912469+VitalijsMuzalovs@users.noreply.github.com>
2022-12-08 21:17:00 +0200

    upd

--- htdocs/Uzņemšanas sistēma/admin/login.php
+++ htdocs/Uzņemšanas sistēma/admin/login.php
@@ -10,12 +10,12 @@
 <body>
 	<div class="container" id="container">
 		<div class="form-container sign-in-container">
-			<form action="#">
+			<form action="#" method="post">
 				<img class="logo" src="../images/lvt.png">
 				<h1>Ielogoties sistēmā</h1>
-				<input type="email" placeholder="Lietotājvārds" />
-				<input type="password" placeholder="Parole" />
-				<button>Ielogoties</button>
+				<input type="email" name="email" placeholder="Lietotājvārds" />
+				<input type="password" name="password" placeholder="Parole" />
+				<button type="submit" name="loginBtn">Ielogoties</button>
 			</form>
 		</div>
 		<div class="overlay-container">
@@ -23,10 +23,40 @@
 				<div class="overlay-panel overlay-right">
 					<h1>Esi sveicināts!</h1>
 					<p>Audzēkņu uzņemšanas sistēmas administrēšanas vietne paredzēta tikai skolas vadībai!</p>
-					<button class="ghost" id="signUp">Doties uz galveno lapu</button>
+					<button class="ghost" id="signUp" type="submit" onclick="toMainPage()">Doties uz galveno lapu</button>
+					<script>
+						function toMainPage() {
+							window.location.href="../index.php";
+						}
+ 					</script>
 				</div>
 			</div>
 		</div>
 	</div>
+	<?php 
+		session_start();
+		if(isset($_POST['loginBtn'])){
+			require("../files/connect_db.php");
+			$userEmail = mysqli_real_escape_string($con,$_POST['email']);
+			$pass = mysqli_real_escape_string($con,$_POST['password']);
+
+			$selUser_SQL = "SELECT * FROM users WHERE email = '$userEmail'";
+			$rsUser = mysqli_query($con,$selUser_SQL);
+				if(mysqli_num_rows($rsUser) == 1){
+					$recordUser = mysqli_fetch_assoc($rsUser);
+					if(password_verify($pass,$recordUser['password'])){
+						$_SESSION["userName"] = $userEmail;
+						
+						header("location: index.php");
+						echo "<script>alert('{$userEmail}')</script>";
+					}else{
+						echo "<script>alert('aaa')</script>";
+						echo "Nepareizs lietotājvārds vai parole!";
+					}
+				}else{
+					echo "Nepareizs lietotājvārds vai parole";
+				}
+			}
+		?>
 </body>
 </html>
--- htdocs/Uzņemšanas sistēma/entry.php
+++ htdocs/Uzņemšanas sistēma/entry.php
@@ -39,32 +39,72 @@
 <div id="pieteiksanas">
     <?php
         if($_SERVER['REQUEST_METHOD'] == 'POST'){
-            # šeit būs daudz PHP koda    
-        ?>
-        <h1>Pieteikšanās specialitātē 
-            <span>
-                <?php $specialitate = $_POST['pieteikties']; echo $specialitate; ?>
-            </span>
-        </h1>
-        <div class="row">
-            <form action="">
-                <input type="text" placeholder="Vārds *" name="vards" class="box1" title="Vārds">
-                <input type="text" placeholder="Uzvārds *" name="uzvards" class="box1" title="Uzvārds">
-                <input type="text" placeholder="Dzimšanas dati *" name="dzimDati" class="box1" title="Dzimšanas dati">
-                <input type="number" placeholder="Tālrunis *" name="talrunis" class="box1" title="Tālrunis">
-                <input type="email" placeholder="E-pasts *" name="epasts" class="box1" title="E-pasts">
-                <input type="text" placeholder="Dzīvesvietas adrese *" name="adrese" class="box1" title="Dzīvesvietas adrese">
-                <input type="text" placeholder="Prioritārā specialitāte *" name="spec1" class="box1" title="Prioritārā specialitāte">
-                <input type="text" placeholder="Sekundārā specialitāte " name="spec2" class="box1" title="Sekundārā specialitāte">
-                <input type="text" placeholder="Iepriekš absolvētā izglītības iestāde *" name="izglitiba" class="box1" title="Iepriekš absolvētā izglītības iestāde">
-                <input type="number" placeholder="Vidējais vērtējums diplomā *" name="vidVertejums" class="box1" title="Vidējais vērtējums diplomā">
-                <input type="text" placeholder="Jūsu komentārs" name="komentars" class="box2" title="Jūsu komentārs">
-                <input type="submit" value="Pieteikties!" class="btn">
-            </form>
-        </div>
-        <?php
-        }else{
-            echo "<div class='zinojums sarkans'>Kaut kas nogāja greizi! ATgriezies sākumlapā un mēģini vēlreiz!</div>";
+            require('files/connect_db.php');
+
+            if(isset($_POST['iesniegtPieteikumu'])){
+                $vards_ievade = mysqli_real_escape_string($con,$_POST['vards']);
+                $uzvards_ievade = mysqli_real_escape_string($con,$_POST['uzvards']);
+                $dzim_dati_ievade = mysqli_real_escape_string($con,$_POST['dzimDati']);
+                $talrunis_ievade = mysqli_real_escape_string($con,$_POST['talrunis']);
+                $epasts_ievade = mysqli_real_escape_string($con,$_POST['epasts']);
+                $adrese_ievade = mysqli_real_escape_string($con,$_POST['adrese']);
+                $spec1_ievade = mysqli_real_escape_string($con,$_POST['spec1']);
+                $spec2_ievade = mysqli_real_escape_string($con,$_POST['spec2']);
+                $izglitiba_ievade = mysqli_real_escape_string($con,$_POST['izglitiba']);
+                $vid_vert_ievade = mysqli_real_escape_string($con,$_POST['vidVertejums']);
+                $komentars_ievade = mysqli_real_escape_string($con,$_POST['komentars']);
+
+                $parbaude_SQL = "SELECT talrunis FROM audzekni WHERE talrunis = '$talrunis_ievade'";
+                $parabudes_rezultats = mysqli_query($con,$parbaude_SQL);
+                $userCount = mysqli_num_rows($parabudes_rezultats);
+
+                if(($userCount >= 1 )){
+                    echo "<div class='zinojums sarkans'>Kļūda! Informācija par Jums jau ir iesniegta!<br> Ja vēlaties veikt kādas izmaiņas - sazinieties ar PIKC LVT uzņemšānas komisiju pa tālruni +371 29999999</div>";
+                }else{
+                    if(!empty($vards_ievade) && !empty($uzvards_ievade) && !empty($dzim_dati_ievade) && !empty($talrunis_ievade) && !empty($epasts_ievade) && !empty($adrese_ievade) && !empty($spec1_ievade) && !empty($spec2_ievade) && !empty($izglitiba_ievade) && !empty($vid_vert_ievade)){        
+                        $pievienot_audzekni_SQL ="
+                            INSERT INTO audzekni 
+                            (vards,uzvards,dzim_dati,talrunis,epasts,adrese,spec1,spec2,izglitiba,vid_vertejums,komentari)
+                            VALUES
+                            ('$vards_ievade','$uzvards_ievade','$dzim_dati_ievade','$talrunis_ievade','$epasts_ievade','$adrese_ievade','$spec1_ievade','$spec2_ievade','$izglitiba_ievade',$vid_vert_ievade,'$komentars_ievade')
+                            ";
+
+                            if(mysqli_query($con,$pievienot_audzekni_SQL)){
+                                echo "<div class='zinojums zals'>Reģistrācija ir noritējusi veiksmīgi! Uz drīzu sazināšanos!</div>";
+                            }else{
+                                echo "<div class='zinojums sarkans'>Reģistrācija nav izdevusies! Mēģiniet vēlreiz!</div>";
+                            }
+                    }else{
+                        echo "<div class='zinojums sarkans'>Reģistrācija nav izdevusies! Kāds no ievades laukiem aizpildīts nekorekti!</div>";
+                        header("Refresh:2;url=./");
+                    }
+                }
+            }else{
+                ?>
+                <h1>Pieteikšanās specialitātē 
+                    <span>
+                        <?php $specialitate = $_POST['pieteikties']; echo $specialitate; ?>
+                    </span>
+                </h1>
+                <div class="row">
+                    <form method="post" action="">
+                        <input type="text" placeholder="Vārds *" name="vards" class="box1" title="Vārds" required>
+                        <input type="text" placeholder="Uzvārds *" name="uzvards" class="box1" title="Uzvārds" required>
+                        <input type="text" placeholder="Dzimšanas dati *" name="dzimDati" class="box1" title="Dzimšanas dati" required>
+                        <input type="number" placeholder="Tālrunis *" name="talrunis" class="box1" title="Tālrunis" required>
+                        <input type="email" placeholder="E-pasts *" name="epasts" class="box1" title="E-pasts" required>
+                        <input type="text" placeholder="Dzīvesvietas adrese *" name="adrese" class="box1" title="Dzīvesvietas adrese" required>
+                        <input type="text" placeholder="Prioritārā specialitāte *" name="spec1" class="box1" title="Prioritārā specialitāte" value = "<?php echo $specialitate ?>" readonly>
+                        <input type="text" placeholder="Sekundārā specialitāte " name="spec2" class="box1" title="Sekundārā specialitāte">
+                        <input type="text" placeholder="Iepriekš absolvētā izglītības iestāde *" name="izglitiba" class="box1" title="Iepriekš absolvētā izglītības iestāde" required>
+                        <input type="number" placeholder="Vidējais vērtējums diplomā *" min="4" max="10" step="0.1" name="vidVertejums" class="box1" title="Vidējais vērtējums diplomā" required>
+                        <input type="text" placeholder="Jūsu komentārs" name="komentars" class="box2" title="Jūsu komentārs">
+                        <input type="submit" value="Pieteikties!" name="iesniegtPieteikumu" class="btn">
+                    </form>
+                </div>
+                <?php
+        }}else{
+            echo "<div class='zinojums sarkans'>Kaut kas nogāja greizi! Atgriezies sākumlapā un mēģini vēlreiz!</div>";
             header("Refresh:2;url=./");
         }
         ?>
