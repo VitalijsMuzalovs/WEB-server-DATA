1d4f4637e3d114864673c2e88a9cf0ce28bdd7dc
VitalijsMuzalovs <102912469+VitalijsMuzalovs@users.noreply.github.com>
2022-12-14 21:15:00 +0200

    upd

--- htdocs/Uzņemšanas sistēma/admin/audzeknis.php
+++ htdocs/Uzņemšanas sistēma/admin/audzeknis.php
@@ -9,46 +9,59 @@
             <?php
                 if($_SERVER['REQUEST_METHOD'] == 'POST'){
                     require "../files/connect_db.php";
-                    $audzeknaID = $_POST['apskatit'];
-                    $atlasit_audzekni_SQL = "SELECT * FROM audzekni a JOIN statuss st ON a.statuss = st.statuss_ID WHERE a.audzeknis_ID = $audzeknaID";
-                    $atlasa_audzekni = mysqli_query($con, $atlasit_audzekni_SQL);
+                    
+                    if(isset($_POST['rediget'])){
+                        $atlasitais_statuss = $_POST['statuss'];
+                        $atjaunot_statusu_SQL = "UPDATE audzekni SET statuss='$atlasitais_statuss' WHERE audzeknis_ID =".$_POST['rediget'];
+                        if(mysqli_query($con,$atjaunot_statusu_SQL)){
+                            echo "<div class='zinojums zals'>Statuss veiksmīgi atjaunots!</div>";
+                            header("Refresh:2; url=audzekni.php");
+                        }else{
+                            echo "<div class='zinojums sarkans'>Kaut kas nogāji greizi! Kļūda sistēmā!</div>";
+                            header("Refresh:2; url=audzekni.php");
+                        }
+                    }else{
+                        $audzeknaID = $_POST['apskatit'];
+                        $atlasit_audzekni_SQL = "SELECT * FROM audzekni a JOIN statuss st ON a.statuss = st.statuss_ID WHERE a.audzeknis_ID = $audzeknaID";
+                        $atlasa_audzekni = mysqli_query($con, $atlasit_audzekni_SQL);
 
-                    $statusi_SQL = "SELECT * FROM statuss";
-                    $atlasa_statusus = mysqli_query($con,$statusi_SQL);
-                    $statusi ="";
+                        $statusi_SQL = "SELECT * FROM statuss";
+                        $atlasa_statusus = mysqli_query($con,$statusi_SQL);
+                        $statusi ="";
 
-                    while($statuss = mysqli_fetch_assoc($atlasa_statusus)){
-                        $statusi = $statusi."<option value='{$statuss['statuss_ID']}'>{$statuss['stat_nosaukums']}</option>";
-                    }
+                        while($statuss = mysqli_fetch_assoc($atlasa_statusus)){
+                            $statusi = $statusi."<option value='{$statuss['statuss_ID']}'>{$statuss['stat_nosaukums']}</option>";
+                        }
 
-                    while($audzeknis = mysqli_fetch_assoc($atlasa_audzekni)){
-                        echo "
-                            <table>
-                                <tr>
-                                    <td rowspan='13'><i class='fas fa-user user-ico'></i>
-                                </tr>
-                                <tr><td>Audzeknis:</td><td class='value'>{$audzeknis['vards']}{$audzeknis['uzvards']}</td></tr>
-                                <tr><td>Dzimšanas dati:</td><td class='value'>{$audzeknis['dzim_dati']}</td></tr>
-                                <tr><td>Tālrunis:</td><td class='value'>{$audzeknis['talrunis']}</td></tr>
-                                <tr><td>E-pasts:</td><td class='value'>{$audzeknis['epasts']}</td></tr>
-                                <tr><td>Dzīvesvietas adrese:</td><td class='value'>{$audzeknis['adrese']}</td></tr>
-                                <tr><td>Prioritārā specialitāte:</td><td class='value'>{$audzeknis['spec1']}</td></tr>
-                                <tr><td>Sekundārā specialitāte:</td><td class='value'>{$audzeknis['spec2']}</td></tr>
-                                <tr><td>Absolvētā izglītības iestāde:</td><td class='value'>{$audzeknis['izglitiba']}</td></tr>
-                                <tr><td>Vidējais vertējums:</td><td class='value'>{$audzeknis['vid_vertejums']}</td></tr>
-                                <tr><td>Reģistrēšanas datums:</td><td class='value'>{$audzeknis['reg_datums']}</td></tr>
-                                <tr><td>Komentāri:</td><td class='value'>{$audzeknis['komentari']}</td></tr>
-                                <tr><td>Uzņemšanas statuss dati:</td><td class='value'>
-                                    <form method='POST'>
-                                        <select name='statuss' required>
-                                            <option value='{$audzeknis['statuss']}' selected hidden>{$audzeknis['stat_nosaukums']}</option>
-                                            $statusi;
-                                        </select>
-                                        <button name='rediget' type='submit' class='btn' value='{$audzeknis['audzeknis_ID']}'>Saglabāt</button>
-                                    </form>
-                                </td></tr>
-                            </table>
-                        ";
+                        while($audzeknis = mysqli_fetch_assoc($atlasa_audzekni)){
+                            echo "
+                                <table>
+                                    <tr>
+                                        <td rowspan='13'><i class='fas fa-user user-ico'></i>
+                                    </tr>
+                                    <tr><td>Audzeknis:</td><td class='value'>{$audzeknis['vards']}{$audzeknis['uzvards']}</td></tr>
+                                    <tr><td>Dzimšanas dati:</td><td class='value'>{$audzeknis['dzim_dati']}</td></tr>
+                                    <tr><td>Tālrunis:</td><td class='value'>{$audzeknis['talrunis']}</td></tr>
+                                    <tr><td>E-pasts:</td><td class='value'>{$audzeknis['epasts']}</td></tr>
+                                    <tr><td>Dzīvesvietas adrese:</td><td class='value'>{$audzeknis['adrese']}</td></tr>
+                                    <tr><td>Prioritārā specialitāte:</td><td class='value'>{$audzeknis['spec1']}</td></tr>
+                                    <tr><td>Sekundārā specialitāte:</td><td class='value'>{$audzeknis['spec2']}</td></tr>
+                                    <tr><td>Absolvētā izglītības iestāde:</td><td class='value'>{$audzeknis['izglitiba']}</td></tr>
+                                    <tr><td>Vidējais vertējums:</td><td class='value'>{$audzeknis['vid_vertejums']}</td></tr>
+                                    <tr><td>Reģistrēšanas datums:</td><td class='value'>{$audzeknis['reg_datums']}</td></tr>
+                                    <tr><td>Komentāri:</td><td class='value'>{$audzeknis['komentari']}</td></tr>
+                                    <tr><td>Uzņemšanas statuss dati:</td><td class='value'>
+                                        <form method='POST'>
+                                            <select name='statuss' required>
+                                                <option value='{$audzeknis['statuss']}' selected hidden>{$audzeknis['stat_nosaukums']}</option>
+                                                $statusi;
+                                            </select>
+                                            <button name='rediget' type='submit' class='btn' value='{$audzeknis['audzeknis_ID']}'>Saglabāt</button>
+                                        </form>
+                                    </td></tr>
+                                </table>
+                            ";
+                        }
                     }
                 }else{
                     echo "<div class='zinojums sarkans'>Kaut kas nogāji greizi! Atgriezies iepriekšējā lapā un mēģini vēlreiz!</div>";
--- htdocs/Uzņemšanas sistēma/admin/index.php
+++ htdocs/Uzņemšanas sistēma/admin/index.php
@@ -5,25 +5,62 @@
 <body>
     <section class="admin">
         <div class="kopsavilkums">
+            <?php require("../files/connect_db.php"); ?>
             <div class="informacija">
-                <span>13</span>
+                <span>
+                    <?php 
+                        $jauni_pieteikumi_SQL = "SELECT COUNT(audzeknis_ID) FROM audzekni WHERE reg_datums BETWEEN NOW() - INTERVAL 1 DAY and NOW();";
+                        $atlasa_jaunako_piet_sk = mysqli_query($con,$jauni_pieteikumi_SQL);
+
+                        while($rezultats = mysqli_fetch_assoc($atlasa_jaunako_piet_sk)){
+                            echo "{$rezultats['COUNT(audzeknis_ID)']}";
+                        }
+                    ?>
+                </span>
                 <h3>Jauni pieteikumi</h3>
                 <p>Pēdējo 24h laikā</p>
             </div>
             <div class="informacija">
-                <span>7</span>
-                <h3>Iesniegti dokumenti</h3>
-                <p>Pēdējo 24h laikā</p>
+                <span>
+                    <?php 
+                        $parbauditi_dok_SQL = "SELECT COUNT(audzeknis_ID) FROM audzekni WHERE statuss = 3;";
+                        $atlasa_parbaudito_dok_sk = mysqli_query($con,$parbauditi_dok_SQL);
+
+                        while($rezultats = mysqli_fetch_assoc($atlasa_parbaudito_dok_sk)){
+                            echo "{$rezultats['COUNT(audzeknis_ID)']}";
+                        }
+                    ?>
+                </span>
+                <h3>Pārbaudīti dokumenti</h3>
+                <p>Kopš uzņemšanas sākuma</p>
             </div>
             <div class="informacija">
-                <span>350</span>
+                <span>
+                    <?php 
+                        $total_docs_SQL = "SELECT COUNT(audzeknis_ID) FROM audzekni;";
+                        $total_doc_count = mysqli_query($con,$total_docs_SQL);
+
+                        while($rezultats = mysqli_fetch_assoc($total_doc_count)){
+                            echo "{$rezultats['COUNT(audzeknis_ID)']}";
+                        }
+                    ?>
+                </span>
                 <h3>Pieteikumu kopā</h3>
                 <p>Kopš uzņemšanas sākuma</p>
             </div>
             <div class="informacija">
-                <span>205</span>
-                <h3>Iesniegti dokumenti</h3>
-                <p>Kopumā no visiem pieteikumiem</p>
+                <span>
+                    <?php 
+                        $active_positions_SQL = "SELECT COUNT(specialitates_ID) FROM specialitates;";
+                        $active_positions_count = mysqli_query($con,$active_positions_SQL);
+
+                        while($rezultats = mysqli_fetch_assoc($active_positions_count)){
+                            echo "{$rezultats['COUNT(specialitates_ID)']}";
+                        }
+                    ?>
+                </span>
+                <h3>Aktīvas specialitātes</h3>
+                <p>Kurām var pieteikties</p>
             </div>
         </div>
 
@@ -32,22 +69,39 @@
                 <div class="head-info">Pēdējās izmaiņas sistēmā:</div>
                 <table>
                     <tr><th>Audzēknis</th><th>Specialitāte</th><th>Statuss</th></tr>
-                    <tr><td>Jānis Bērziņš</td><td>Pavārs</td><td>Procesā</td></tr>
-                    <tr><td>Jānis Bērziņš</td><td>Pavārs</td><td>Procesā</td></tr>
-                    <tr><td>Jānis Bērziņš</td><td>Pavārs</td><td>Procesā</td></tr>
-                    <tr><td>Jānis Bērziņš</td><td>Pavārs</td><td>Procesā</td></tr>
-                    <tr><td>Jānis Bērziņš</td><td>Pavārs</td><td>Procesā</td></tr>
+                    <?php
+                        $last_changes_SQL = "SELECT * FROM audzekni a JOIN statuss st ON a.statuss = st.statuss_ID ORDER BY reg_datums DESC LIMIT 7;";
+                        $last_changes = mysqli_query($con,$last_changes_SQL);
+
+                        while($rezultats = mysqli_fetch_assoc($last_changes)){
+                            echo "
+                                <tr>
+                                    <td>{$rezultats['vards']} {$rezultats['uzvards']}</td>
+                                    <td>{$rezultats['spec1']}</td>
+                                    <td>{$rezultats['stat_nosaukums']}</td>
+                                </tr>
+                            ";
+                        }
+                    ?>
                 </table>
             </div>
             <div class="info2">
                 <div class="head-info">Pieprasītākās specialitātes:</div>
                 <table>
                     <tr><th>Specialitāte</th><th>Pieteikumi</th></tr>
-                    <tr><td>Programmēšanas tehniķis</td><td>76</td></tr>
-                    <tr><td>Datorisistēmu tehniķis</td><td>28</td></tr>
-                    <tr><td>Pavārs</td><td>26</td></tr>
-                    <tr><td>Mehatronisku sistēmu tehniķis</td><td>25</td></tr>
-                    <tr><td>Grāmatvedis</td><td>25</td></tr>
+                    <?php
+                        $top_positions_SQL = "SELECT spec1,COUNT(spec1) FROM audzekni GROUP BY spec1 ORDER BY COUNT(spec1) DESC LIMIT 7;";
+                        $top_positions = mysqli_query($con,$top_positions_SQL);
+
+                        while($rezultats = mysqli_fetch_assoc($top_positions)){
+                            echo "
+                                <tr>
+                                    <td>{$rezultats['spec1']}</td>
+                                    <td>{$rezultats['COUNT(spec1)']}</td>
+                                </tr>
+                            ";
+                        }
+                    ?>
                 </table>
             </div>
         </div>
